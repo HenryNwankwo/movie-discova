5e0af3f8d738614715e059d97221b8e76c9baaf8
Nwankwo Henry <nameisifunanya@gmail.com>
2023-09-11 18:34:37 +0100

    Styled the hero section

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -1,9 +1,46 @@
+import { FaCirclePlay } from 'react-icons/fa6';
 import Header from './Header';
+import Image from 'next/image';
 
 function HeroSection() {
   return (
-    <section className='hero-section'>
+    <section className='mbox-hero-section'>
       <Header></Header>
+      <article className='mbox-hero-featured-movie'>
+        <h1 className='mbox-hero-title'>John Wick 3: Parabellum</h1>
+        <div className='mbox-hero-movie-rating'>
+          <p className='flex items-center mr-5'>
+            <Image
+              src='./assets/imdb.svg'
+              alt='imdb image'
+              width={30}
+              height={30}
+              className='mr-1.5'
+            />{' '}
+            79.0/100
+          </p>
+          <p className='flex items-center'>
+            <Image
+              src='./assets/tomato.svg'
+              alt=''
+              width={15}
+              height={15}
+              className='mr-1.5'
+            />{' '}
+            71%
+          </p>
+        </div>
+        <p className='mbox-hero-movie-description'>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          dolor sequi, velit repellat recusandae quis officia eaque sint nobis
+          ipsum alias corporis quasi quae, provident libero inventore ratione,
+          incidunt aspernatur.
+        </p>
+        <button className='mbox-hero-btn'>
+          <FaCirclePlay className='text-white mr-2' />
+          WATCH TRAILER
+        </button>
+      </article>
     </section>
   );
 }
