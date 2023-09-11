c9b9e18dfc3d1462dfc8a7d9187e67fb06e4b266
Nwankwo Henry <nameisifunanya@gmail.com>
2023-09-11 11:27:54 +0100

    Created a movie card component, featured movies structure and styles

--- components/FeaturedMoviesSection.jsx
+++ components/FeaturedMoviesSection.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import MovieCard from './MovieCard';
+import { FaChevronRight } from 'react-icons/fa6';
+import Link from 'next/link';
 
 function FeaturedMoviesSection() {
-  return <section className='mbox-movies-section'></section>;
+  return (
+    <section className='mbox-movies-section'>
+      <div className='mbox-movies-section-head'>
+        <h3 className='font-bold text-2xl'>Featured Movie</h3>
+        <Link
+          href='#'
+          className='text-pink-700 flex items-center text-left font-semibold'
+        >
+          See more <FaChevronRight className='text-lg ml-1' />
+        </Link>
+      </div>
+      <MovieCard></MovieCard>
+    </section>
+  );
 }
 
 export default FeaturedMoviesSection;
---
+++ components/MovieCard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function MovieCard() {
+  return <article></article>;
+}
+
+export default MovieCard;
