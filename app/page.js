1d4fdd6cabe515fb5399ac8ea5c6dfde718f1dee
Nwankwo Henry <nameisifunanya@gmail.com>
2023-09-11 17:31:47 +0100

    Added some image assets

--- app/page.js
+++ app/page.js
@@ -1,50 +1,11 @@
 import FeaturedMoviesSection from '@/components/FeaturedMoviesSection';
 import Footer from '@/components/Footer';
-import Image from 'next/image';
-import Link from 'next/link';
-import { BiSearch } from 'react-icons/bi';
-import { HiOutlineMenuAlt4 } from 'react-icons/hi';
+import HeroSection from '@/components/HeroSection';
 
 export default function Home() {
   return (
     <main className=''>
-      <section className='hero-section'>
-        <header>
-          <article className='mbox-logo-group'>
-            <Image
-              src=''
-              alt='MovieBox Logo'
-              width={100}
-              height={100}
-              className='mr-3'
-            />
-            MovieBox
-          </article>
-          <article className='mbox-search-group'>
-            <form action=''>
-              <input
-                type='text'
-                name='search'
-                id='search'
-                className=''
-                placeholder='What do you want to watch?'
-              />
-              <label htmlFor='search'>
-                {' '}
-                <BiSearch className='text-lg' />{' '}
-              </label>
-            </form>
-          </article>
-          <article>
-            <Link href='#' className='mbox-sign-in-link'>
-              Sign in
-            </Link>
-            <div className='mbox-menu-burger'>
-              <HiOutlineMenuAlt4 className='text-lg' />
-            </div>
-          </article>
-        </header>
-      </section>
+      <HeroSection></HeroSection>
       <FeaturedMoviesSection></FeaturedMoviesSection>
       <Footer></Footer>
     </main>
