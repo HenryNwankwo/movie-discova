1d6f1616b6c2229557a13f801c10fb2545e9957c
Nwankwo Henry <nameisifunanya@gmail.com>
2023-09-11 17:31:07 +0100

    created a hero section component

---
+++ components/HeroSection.jsx
@@ -0,0 +1,11 @@
+import Header from './Header';
+
+function HeroSection() {
+  return (
+    <section className='hero-section'>
+      <Header></Header>
+    </section>
+  );
+}
+
+export default HeroSection;
