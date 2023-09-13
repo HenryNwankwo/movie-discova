e7d863748f01db0dbeebbd8b953c62db1b25346e
Nwankwo Henry <nameisifunanya@gmail.com>
2023-09-13 08:58:57 +0100

    Added the remaining icons to movie cast component

--- components/MovieCast.jsx
+++ components/MovieCast.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { BsChevronDown } from 'react-icons/bs';
+import { BsChevronDown, BsListUl } from 'react-icons/bs';
 
 function MovieCast() {
   return (
@@ -28,20 +28,28 @@ function MovieCast() {
           </span>
         </p>
         <div className='flex items-center justify-between'>
-          <button className='p-2 px-4 bg-pink-600 rounded-md text-white'>
+          <button className='p-2 px-4 bg-pink-600 hover:bg-pink-500 rounded-md text-white'>
             Top rated movie #65
           </button>
-          <button className='flex item-center border-[1px] border-neutral-600 rounded-md p-2 px-4'>
+          <button className='flex item-center border-[1px] border-neutral-600 bg-white hover:bg-pink-50 rounded-md p-2 px-4'>
             Awards 9 nomination
             <BsChevronDown className='ml-2 text-lg' />
           </button>
         </div>
       </article>
       <article className='movie-ad-group'>
-        <button className='w-full my-1 p-2 px-4 rounded-md bg-pink-600 hover:bg-pink-500 text-white'>
+        <button className='flex items-center justify-center w-full my-1 p-2 px-4 rounded-md bg-pink-600 hover:bg-pink-500 text-white'>
+          <Image
+            src={'/assets/TwoTickets.svg'}
+            width={25}
+            height={25}
+            alt='see showtimes icon'
+            className='mr-3'
+          />
           See showtimes
         </button>
-        <button className='w-full my-1 p-2 px-4 rounded-md bg-pink-50 hover:bg-pink-100 border-[1px] border-pink-600'>
+        <button className='flex items-center justify-center w-full mt-2 p-2 px-4 rounded-md bg-pink-50 hover:bg-pink-100 border-[1px] border-pink-600'>
+          <BsListUl className='text-lg mr-3' />
           More watch options
         </button>
         <div className='w-full relative h-44'>
@@ -52,7 +60,8 @@ function MovieCast() {
             alt='Movie ad image'
             className='mt-3'
           />
-          <p className='absolute w-full text-white text-xs bottom-2 bg-black bg-opacity-40 p-2'>
+          <p className='absolute flex items-center w-full text-white text-[0.7rem] bottom-2 bg-black bg-opacity-40 p-2'>
+            <BsListUl className='text-lg mr-2' />
             The Best Movies and Shows in September
           </p>
         </div>
