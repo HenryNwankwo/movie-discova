0169c2587b539bfe4f96a8c131ba68e296b8ea83
Nwankwo Henry <nameisifunanya@gmail.com>
2023-09-15 13:29:19 +0100

    Changed axios to fetch method in getting movies by id

--- components/MoviesMainPage.jsx
+++ components/MoviesMainPage.jsx
@@ -1,33 +1,49 @@
 'use client';
 import MovieCast from './MovieCast';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import MovieInfoGroup from './MovieInfoGroup';
 import MoviePreview from './MoviePreview';
 
 function MoviesMainPage({ id }) {
-  const [movieDetails, setMovieDetails] = useState({});
+  const [movieData, setMovieData] = useState({});
+  const [loading, setLoading] = useState(true);
   const API_KEY = process.env.NEXT_PUBLIC_DB_API_KEY;
   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
 
   // fetching movie details
-  useEffect(() => {
-    async function fetchMovie() {
-      try {
-        const resp = await axios.get(url);
-        setMovieDetails(resp.data);
-        console.log(resp.data);
-      } catch (error) {
+  const getMovieDetails = () => {
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response is not ok!');
+        }
+
+        return response.json();
+      })
+      .then((data) => {
+        setMovieData(data);
+        setLoading(false);
+        console.log('movie details:', movieData);
+        console.log('Data.results:', data);
+      })
+      .catch((error) => {
         console.log('Error Message: ', error);
-      }
-    }
-    fetchMovie();
+      });
+  };
+
+  useEffect(() => {
+    getMovieDetails();
   }, [id]);
-  return (
+
+  return loading ? (
+    <div className='w-full h-screen justify-center items-center bg-red-200'>
+      Loading...
+    </div>
+  ) : (
     <>
       <MoviePreview></MoviePreview>
-      <MovieInfoGroup movieDetails={movieDetails}></MovieInfoGroup>
-      <MovieCast movieDetails={movieDetails}></MovieCast>
+      <MovieInfoGroup movieDetails={movieData}></MovieInfoGroup>
+      <MovieCast movieDetails={movieData}></MovieCast>
     </>
   );
 }
